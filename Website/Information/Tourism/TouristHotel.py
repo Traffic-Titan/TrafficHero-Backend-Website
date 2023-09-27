fb90dc50f2cfec198ce13d853a0e60b39b3469ed
ZiJie <zsp97118@gmail.com>
2023-09-27 15:59:19 +0800

    feat: [Update]觀光旅遊相關API

---
+++ Website/Information/Tourism/TouristHotel.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import Service.Token as Token
+from Main import MongoDB # 引用MongoDB連線實例
+import Service.TDX as TDX
+
+router = APIRouter(tags=["5.觀光資訊(Website)"],prefix="/Website/Information/Tourism")
+collection = MongoDB.getCollection("traffic_hero","tourism_tourist_hotel")
+@router.put("/TouristHotel",summary="【Update】觀光景點-全臺觀光景點飯店")
+async def TouristHotel(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    """
+    一、資料來源: \n
+            1. https://tdx.transportdata.tw/api-service/swagger/basic/cd0226cf-6292-4c35-8a0d-b595f0b15352#/Tourism/TourismApi_Hotel_2244 \n
+    二、Input \n
+            1. 
+    三、Output \n
+            1. 
+    四、說明 \n
+            1.
+    """
+    Token.verifyToken(token.credentials,"user") # JWT驗證
+    
+    collection.drop() # 刪除該collection所有資料
+    
+    dataToDatabase()
+
+    return f"已更新筆數:{collection.count_documents({})}"
+
+def dataToDatabase():
+    try:
+        url = f"https://tdx.transportdata.tw/api/basic/v2/Tourism/Hotel?%24format=JSON" # 取得資料來源網址
+        data = TDX.getData(url) # 取得資料
+        collection.insert_many(data) # 將資料存入MongoDB
+    except Exception as e:
+        print(e)
