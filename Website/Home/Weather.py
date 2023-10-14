ea6638fe9f2c869ce4730fea62eabb948a198679
Eddie <zxc1435693@gmail.com>
2023-10-14 22:52:24 +0800

    feat: 修改天氣狀態圖示存取方式

--- Website/Home/Weather.py
+++ Website/Home/Weather.py
@@ -10,14 +10,14 @@ from pydantic import BaseModel
 router = APIRouter(tags=["1.首頁(Website)"],prefix="/Website/Home")
 
 class weather_icon(BaseModel):
-    type: str
-    weather: str
-    icon_url: str
+    id: str
+    icon_url_day: str
+    icon_url_night: str
     
 collection = MongoDB.getCollection("traffic_hero","weather_icon")
 
 @router.get("/Weather/Icon", summary="【Read】首頁-天氣狀態圖示")
-async def getWeatherIcon(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+async def getWeatherIcon(type1: str = "all", token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
     """
     一、資料來源: \n
             1. 
@@ -30,32 +30,18 @@ async def getWeatherIcon(token: HTTPAuthorizationCredentials = Depends(HTTPBeare
     """
     Token.verifyToken(token.credentials,"admin") # JWT驗證
     
-    result = collection.find({},{"_id":0})
+    match type1:
+        case "all":
+                result = collection.find({},{"_id":0})
+        case "晴":
+                result = collection.find({"type1": "晴"}, {"_id":0})
+        case "多雲":
+                result = collection.find({"type1": "多雲"}, {"_id":0})
+        case "陰":
+                result = collection.find({"type1": "陰"}, {"_id":0})
     
     return list(result)
 
-@router.post("/Weather/Icon", summary="【Create】首頁-天氣狀態圖示")
-async def createWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
-    """
-    一、資料來源: \n
-            1. 
-    二、Input \n
-            1. 
-    三、Output \n
-            1. 
-    四、說明 \n
-            1.
-    """
-    Token.verifyToken(token.credentials,"admin") # JWT驗證
-    
-    if collection.find_one({"type": data.type, "weather": data.weather}) != None:
-        return {"message":f"新增失敗，已有此類別與天氣狀態 ({data.type}-{data.weather})"}
-    else:
-        collection.insert_one(data.dict())
-    
-    return {"message":"新增成功"}
-    
-    
 @router.put("/Weather/Icon", summary="【Update】首頁-天氣狀態圖示")
 async def updateWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
     """
@@ -70,24 +56,45 @@ async def updateWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredenti
     """
     Token.verifyToken(token.credentials,"admin") # JWT驗證
     
-    collection.update_one({"type": data.type, "weather": data.weather},{"$set":{ "icon_url": data.icon_url}})
+    collection.update_one({"id": data.id},{"$set":{ "icon_url_day": data.icon_url_day,"icon_url_night": data.icon_url_night}})
     
     return {"message":"更新成功"}
     
-@router.delete("/Weather/Icon", summary="【Delete】首頁-天氣狀態圖示")
-async def deleteWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
-    """
-    一、資料來源: \n
-            1. 
-    二、Input \n
-            1. 
-    三、Output \n
-            1. 
-    四、說明 \n
-            1.
-    """
-    Token.verifyToken(token.credentials,"admin") # JWT驗證
+# @router.delete("/Weather/Icon", summary="【Delete】首頁-天氣狀態圖示")
+# async def deleteWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+#     """
+#     一、資料來源: \n
+#             1. 
+#     二、Input \n
+#             1. 
+#     三、Output \n
+#             1. 
+#     四、說明 \n
+#             1.
+#     """
+#     Token.verifyToken(token.credentials,"admin") # JWT驗證
     
-    collection.delete_one({"type": data.type, "weather": data.weather})
+#     collection.delete_one({"type": data.type, "weather": data.weather})
     
-    return {"message":"刪除成功"}
+#     return {"message":"刪除成功"}
+
+# @router.post("/Weather/Icon", summary="【Create】首頁-天氣狀態圖示")
+# async def createWeatherIcon(data: weather_icon, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+#     """
+#     一、資料來源: \n
+#             1. 
+#     二、Input \n
+#             1. 
+#     三、Output \n
+#             1. 
+#     四、說明 \n
+#             1.
+#     """
+#     Token.verifyToken(token.credentials,"admin") # JWT驗證
+    
+#     if collection.find_one({"type": data.type, "weather": data.weather}) != None:
+#         return {"message":f"新增失敗，已有此類別與天氣狀態 ({data.type}-{data.weather})"}
+#     else:
+#         collection.insert_one(data.dict())
+    
+#     return {"message":"新增成功"}
