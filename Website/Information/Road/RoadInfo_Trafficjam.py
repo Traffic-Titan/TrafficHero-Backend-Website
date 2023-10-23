794026dde7724aae0e3ded7e7f737287bf885f56
ZiJie <zsp97118@gmail.com>
2023-10-24 00:16:30 +0800

    feat:讀取PBS以更新道路資訊

---
+++ Website/Information/Road/RoadInfo_Trafficjam.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import Service.Token as Token
+from Main import MongoDB # 引用MongoDB連線實例
+import Service.TDX as TDX
+import requests
+
+router = APIRouter(tags=["4-1.道路資訊(Website)"],prefix="/Website/Information/Road")
+collection = MongoDB.getCollection("traffic_hero","information_road_info_pbs_trafficjam")
+
+@router.put("/RoadInfo_Trafficjam",summary="【Update】道路資訊-PBS-壅塞")
+async def RoadInfo_Trafficjam(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    """
+    一、資料來源: \n
+            1. 警廣即時路況\n
+            	https://data.gov.tw/dataset/15221 \n
+    二、Input \n
+            1. 
+    三、Output \n
+            1. 
+    四、說明 \n
+            1.
+    """
+    Token.verifyToken(token.credentials,"admin") # JWT驗證    
+
+    return updateInfo()
+
+def updateInfo():
+    collection.drop() # 刪除該collection所有資料
+    documents = []
+
+    try:
+        url = "https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=36384FA8-FACF-432E-BB5B-5F015E7BC1BE"
+        response = requests.get(url)
+        PBS_data = response.json()
+
+        for data in PBS_data:
+            if(data['roadtype'] == "阻塞"):
+                documents.append(data)
+    except Exception as e:
+        print(e)
+         
+    collection.insert_many(documents)
+
+    return f"已更新筆數:{collection.count_documents({})}"
