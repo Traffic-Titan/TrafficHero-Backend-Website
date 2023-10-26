602c2f5f99b829a99ebb694d1d7ac8edc1fad5c2
Eddie <zxc1435693@gmail.com>
2023-10-26 23:07:18 +0800

    feat: 新增快速尋找地點資料更新功能

--- Website/Home/QuickSearch/ConvenientStore.py
+++ Website/Home/QuickSearch/ConvenientStore.py
@@ -5,10 +5,11 @@ from Main import MongoDB # 引用MongoDB連線實例
 from bson import ObjectId
 import Service.TDX as TDX
 import pandas as pd
+from geopy.geocoders import Nominatim
 
 router = APIRouter(tags=["1.首頁(Website)"],prefix="/Website/Home")
 
-@router.put("/QuickSearch/ConvenientStore",summary="【Update】快速尋找地點-便利商店列表")
+@router.put("/QuickSearch/ConvenientStore(DEV)",summary="【Update】快速尋找地點-便利商店列表(DEV)")
 async def updateConvenientStoreListAPI(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
         """
         一、資料來源: \n
@@ -37,11 +38,41 @@ def updateConvenientStoreList():
                 # 將DataFrame轉換為字典
                 records = df.to_dict(orient='records')
 
+                documents = []
+                count = 0
+                for r in records:
+                        count += 1
+                        print(f"更新第{count}筆資料")
+                        # r["公司統一編號"] = int(r["公司統一編號"])
+                        # r["公司名稱"] = r["公司名稱"].strip()
+                        # r["分公司統一編號"] = int(r["分公司統一編號"])
+                        # r["分公司名稱"] = r["分公司名稱"].strip()
+                        # r["分公司地址"] = r["分公司地址"].strip()
+                        geolocator = Nominatim(user_agent="your_app_name")
+                        
+                        
+                        if int(r["分公司狀態"]) == 1: # 核准設立
+                                r["分公司狀態"] = "核准設立"
+                                print(r["分公司地址"])
+                                # url = f"https://tdx.transportdata.tw/api/advanced/V3/Map/GeoCode/Coordinate/Address/{r['分公司地址']}?%24format=GEOJSON"
+                                # result = TDX.getData(url)
+                                
+                                location = geolocator.geocode(r["分公司地址"])
+                                if location:
+                                        # r["position"] = {"longitude": result['features'][0]['geometry']['coordinates'][0], "latitude": result['features'][0]['geometry']['coordinates'][0]}
+                                        r["position"] = {"longitude": location.longitude, "latitude": location.latitude}
+                                        r["status"] = True
+                                else:
+                                        r["status"] = False                        
+                        
+                        documents.append(r)
+                        print(r)                             
                 # 刪除舊的collection資料
                 collection.drop()
 
                 # 插入新的資料
-                collection.insert_many(records)
+                collection.insert_many(documents)
                 return {"message": f"更新成功，總筆數:{collection.count_documents({})}"}
         except Exception as e:
                 return {"message": f"更新失敗，錯誤訊息:{e}"}
+                
